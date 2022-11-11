9dffd75b1a2623fdc1189922e184a44ea1187473
Tiago Castro <tiagolobocastro@gmail.com>
2022-11-11 00:02:31 +0000

    style: fix formatting of non-related files
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>
    Signed-off-by: Dmitry Savitskiy <dmitry.savitskiy@datacore.com>

--- lib/bdev/bdev.c
+++ lib/bdev/bdev.c
@@ -4604,7 +4604,7 @@ static int
 bdev_read_blocks_with_md(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch, void *buf,
 			 void *md_buf, uint64_t offset_blocks, uint64_t num_blocks,
 			 spdk_bdev_io_completion_cb cb, void *cb_arg,
-                         uint32_t ext_io_flags)
+			 uint32_t ext_io_flags)
 {
 	struct spdk_bdev *bdev = spdk_bdev_desc_get_bdev(desc);
 	struct spdk_bdev_io *bdev_io;
@@ -4647,8 +4647,8 @@ spdk_bdev_read(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 
 int
 spdk_bdev_read_with_flags(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
-                          void *buf, uint64_t offset, uint64_t nbytes,
-                          spdk_bdev_io_completion_cb cb, void *cb_arg,
+			  void *buf, uint64_t offset, uint64_t nbytes,
+			  spdk_bdev_io_completion_cb cb, void *cb_arg,
 			  uint32_t ext_io_flags)
 {
 	uint64_t offset_blocks, num_blocks;
@@ -4658,7 +4658,8 @@ spdk_bdev_read_with_flags(struct spdk_bdev_desc *desc, struct spdk_io_channel *c
 		return -EINVAL;
 	}
 
-	return spdk_bdev_read_blocks_with_flags(desc, ch, buf, offset_blocks, num_blocks, cb, cb_arg, ext_io_flags);
+	return spdk_bdev_read_blocks_with_flags(desc, ch, buf, offset_blocks, num_blocks, cb, cb_arg,
+						ext_io_flags);
 }
 
 int
@@ -4671,11 +4672,12 @@ spdk_bdev_read_blocks(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
 
 int
 spdk_bdev_read_blocks_with_flags(struct spdk_bdev_desc *desc, struct spdk_io_channel *ch,
-                                 void *buf, uint64_t offset_blocks, uint64_t num_blocks,
-                                 spdk_bdev_io_completion_cb cb, void *cb_arg,
-			         uint32_t ext_io_flags)
+				 void *buf, uint64_t offset_blocks, uint64_t num_blocks,
+				 spdk_bdev_io_completion_cb cb, void *cb_arg,
+				 uint32_t ext_io_flags)
 {
-	return bdev_read_blocks_with_md(desc, ch, buf, NULL, offset_blocks, num_blocks, cb, cb_arg, ext_io_flags);
+	return bdev_read_blocks_with_md(desc, ch, buf, NULL, offset_blocks, num_blocks, cb, cb_arg,
+					ext_io_flags);
 }
 
 int
